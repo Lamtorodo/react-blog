61d2b99f94da8e0052c9cb75171b019f4bc61137
Lamtorodo <mamoudoutokocel@gmail.com>
2021-06-05 18:00:56 +0200

    adding state of hook

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,17 @@
+import {useState} from 'react'
 const Home = () => {
     const title = "Welcome to our Website"
+    const [name,setName] = useState('mario')
     const handleClick = (name , e) => {
         console.log('hello ' + name , e.target)
+        setName('Lamtoro')
     }
     return (
         <div className="home">
             <h1>
                 {title}
             </h1>
+            <p>{name}</p>
             <button onClick={ (e) => handleClick('Lamtoro',e)}>Click Me</button>
         </div>
     );
